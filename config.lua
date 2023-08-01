9c10131caa6b3ce2308647ba615a1fff4c0e1911
Timey <operationhope202@gmail.com>
2023-08-01 15:35:01 -0400

    fixed the lua

---
+++ config.lua
@@ -0,0 +1,11 @@
+function love.conf(t)
+    t.identity = nil
+    t.console = false
+    t.window.title = "Bomb Game by Timey"
+    t.window.icon = "icon.jpg"
+    t.window.width = 1280--1280----1920
+    t.window.height = 720--720-----1080
+    t.window.fullscreen = false
+    t.window.resizable = false
+    t.window.msaa = 2
+end

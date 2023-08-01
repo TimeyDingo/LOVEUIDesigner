46961b7972804f1ff983bb3eb20855fdfdf38b7b
Timey <operationhope202@gmail.com>
2023-08-01 15:38:07 -0400

    fixed files finally

--- conf.lua
+++ conf.lua
@@ -1,10 +1,9 @@
 function love.conf(t)
     t.identity = nil
     t.console = false
-    t.window.title = "Bomb Game by Timey"
-    t.window.icon = "icon.jpg"
-    t.window.width = 1280--1280----1920
-    t.window.height = 720--720-----1080
+    t.window.title = "UI Designer by Timey"
+    t.window.width = 200--1280----1920
+    t.window.height = 300--720-----1080
     t.window.fullscreen = false
     t.window.resizable = false
     t.window.msaa = 2
